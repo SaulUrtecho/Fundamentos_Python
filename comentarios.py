c043969766089183c3e949d288302507af300ce2
SaulUrtecho <saul.urtecho93@gmail.com>
2021-08-17 14:16:53 -0500

    Subimos el primer modulo

---
+++ comentarios.py
@@ -0,0 +1,9 @@
+
+
+# Este es un comentario de una sola linea
+
+''' Este es un comentario 
+    de varias lineas de 
+    codigo, tambien sirve para documentar
+    alguna clase, modulo o funcion especifica y acceder a
+    esta mediante el nombre y la funcion especial name.__doc__ '''

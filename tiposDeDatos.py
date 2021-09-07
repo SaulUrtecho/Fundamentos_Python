169e12eb2fcf798ef35286b699d4032707ad800a
SaulUrtecho <saul.urtecho93@gmail.com>
2021-09-07 14:27:16 -0500

    Se sube archivo de tipos de datos

---
+++ tiposDeDatos.py
@@ -0,0 +1,22 @@
+# NUMERICOS
+
+numInt = 5
+print(type(numInt)) # imprime tipo entero
+
+numFloat = 0.347
+print(type(numFloat)) # imprime tipo flotante
+
+# CADENA
+
+cadena = 'Mi nombre es saul' # cadena de comillas simples
+cadena2 = "Mi segundo nombre es Alejandro" # cadena de comillas dobles
+print(type(cadena))
+print(type(cadena2))
+
+# BOOLEANOS
+
+verdadero = True
+falso = False
+
+print(type(verdadero))
+print(type(falso))
